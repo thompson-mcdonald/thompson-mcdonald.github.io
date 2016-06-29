91cb6b646ec135c5c1529ca0310d379592384531
jacob thompson <thampsen@gmail.com>
2016-06-29 16:39:05 +0100

    wrote logic for drag and drop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,20 @@
 var $ = require('jquery');
+
 require('jquery-ui');
 
 $(document).ready(function(){
-
+	$('.collage-area img').each(function(){
+		$(this).addClass('pos-absolute');
+		$(this).css({
+			"left": (Math.random() * 80 + "%"),
+			"top": (Math.random() * 80 + "%"),
+			"right": (Math.random() * 80 + "%"),
+			"bottom": (Math.random() * 80 + "%")
+		})
+		$(this).draggable({
+			drag: function(){
+				$(this).addClass('z-index-mid').siblings().removeClass('z-index-mid');
+			}
+		});
+	});
 });
